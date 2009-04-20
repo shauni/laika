60e9c15c5c9afe086312809833019a2e03fd60f6
Zack Hobson <zack@opensourcery.com>
2009-04-20 15:23:58 -0700

    protect_from_forgery no longer accepts :secret param.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,8 +14,7 @@ class ApplicationController < ActionController::Base
   before_filter :login_required, :except => [:login, :signup, :forgot_password, :reset_password]
 
   # See ActionController::RequestForgeryProtection for details
-  # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery :secret => 'dece9bb4d13101130349c3bef2c45b37'
+  protect_from_forgery
   
   protected
 
