97ff25222e8470003581bda6d1e3032662fd7461
Zack Hobson <zack@opensourcery.com>
2009-04-27 17:39:32 -0700

    Cleaned up VendorTestPlan#export_csv.

--- app/models/vendor_test_plan.rb
+++ app/models/vendor_test_plan.rb
@@ -70,62 +70,48 @@ class VendorTestPlan < ActiveRecord::Base
 
   # Build and return a CSV document with the contents of the given user's test plan.
   #
-  # FIXME this needs to be rewritten
+  # FIXME this (still) needs to be rewritten
+  #
+  # XXX also, does anyone use this? the output isn't even really CSV
   def self.export_csv(user)
     vendor_test_plans = user.vendor_test_plans
-    users = User.find(:all)
     vendors = Vendor.find(:all)
-    kinds = Kind.find(:all)
 
-    report = ""
+    report = "#{user.first_name} #{user.last_name}'s Inspections:\n"
 
-    users.each do |user|
-      flag1 = 0
-      vendors.each do |vendor|
-        flag2 = 0
-        vendor_test_plans.each do |vendor_test_plan|
-          if vendor_test_plan.user_id == user.id	  				
-            if vendor_test_plan.vendor_id == vendor.id
-              if flag1 == 0
-                report << user.first_name + " " + user.last_name + "'s Inspections:\n"
-                flag1 = 1
-              end	
-              if flag2 == 0
-                report << "Vendor ID:,"
-                report << vendor.public_id + "\n\n"	
-                flag2 = 1
-              end	
-              report << "Patient Name:,"
-              report << vendor_test_plan.patient.name + "\n" 	
-              kinds.each do |kind|
-                if kind.id == vendor_test_plan.kind_id
-                  report << "Inspection Type:,"
-                  report << kind.name + "\n"
-                  break
-                end
-              end
-              report << "Result:,"
-              error_count = 0
-              if vendor_test_plan.clinical_document
-                error_count = vendor_test_plan.content_errors.length	        	          
-                if error_count > 0
-                  report << "failure\n"
-                  report << "Number of errors:, "
-                  report << error_count.to_s
-                  report << "\n"
-                else
-                  report << "success\n"
-                  report << "Number of errors:, --\n"
-                end
-              else
-                report << "in progress\n"
-                report << "Number of errors:, --\n"		  				
-              end
-              report << "Last Modified:,"
-              report << vendor_test_plan.updated_at.strftime("%d.%b.%Y")
-              report << "\n\n"
+    vendors.each do |vendor|
+      seen_vendor = false
+      vendor_test_plans.each do |vendor_test_plan|
+        if vendor_test_plan.vendor_id == vendor.id
+          if not seen_vendor
+            report << "Vendor ID:,"
+            report << vendor.public_id + "\n\n"	
+            seen_vendor = true
+          end	
+          report << "Patient Name:,"
+          report << vendor_test_plan.patient.name + "\n" 	
+          report << "Inspection Type:,"
+          report << vendor_test_plan.kind.name + "\n"
+          report << "Result:,"
+          error_count = 0
+          if vendor_test_plan.clinical_document
+            error_count = vendor_test_plan.content_errors.length	        	          
+            if error_count > 0
+              report << "failure\n"
+              report << "Number of errors:, "
+              report << error_count.to_s
+              report << "\n"
+            else
+              report << "success\n"
+              report << "Number of errors:, --\n"
             end
+          else
+            report << "in progress\n"
+            report << "Number of errors:, --\n"		  				
           end
+          report << "Last Modified:,"
+          report << vendor_test_plan.updated_at.strftime("%d.%b.%Y")
+          report << "\n\n"
         end
       end
     end
