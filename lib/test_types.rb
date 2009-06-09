650be2c165bf4354a015c0505a3b076727d142fa
Zack Hobson <zack@opensourcery.com>
2009-06-09 09:34:03 -0700

    Perform assignment using test types for all kinds.
    Assignment is now done using the new test types facility. The only types
    that need assignment callbacks are the XDS tests.

--- lib/test_types.rb
+++ lib/test_types.rb
@@ -3,13 +3,29 @@ TestType.register("C32 Display and File")
 
 TestType.register("C32 Generate and Format")
 
+TestType.register("PDQ Query")
+
+TestType.register("PIX Feed")
+
+TestType.register("PIX Query")
+
 TestType.register("XDS Provide and Register") do
   assign do |vendor_test_plan|
     vendor_test_plan.metadata = params[:metadata]
-    vendor_test_plan.save
+    vendor_test_plan.save!
 
     @metadata = params[:metadata]
     render 'xds_patients/assign_success'
+  end
+end
+
+TestType.register("XDS Query and Retrieve") do
+  assign do |vendor_test_plan|
+    vendor_test_plan.metadata = params[:metadata]
+    doc = XDSUtils.retrieve_document(vendor_test_plan.metadata)
+    vendor_test_plan.clinical_document = ClinicalDocument.new(:uploaded_data=>doc)
+    vendor_test_plan.clinical_document.save!
+    vendor_test_plan.save!
   end
 end
 
