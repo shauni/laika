a59615e33b2cdc9f1192ae77c0e4f553728fbe8b
Goss Nuzzo-Jones <gossnj@mm139532-pc.mitre.org>
2009-08-11 14:05:38 -0400

    minor cleanup of code

--- app/controllers/xds_utility_controller.rb
+++ app/controllers/xds_utility_controller.rb
@@ -24,29 +24,16 @@ class XdsUtilityController < ApplicationController
           documents = XdsUtility.documents( patient[ 'identificationscheme' ], patient['value'] )
           @documents[ patient ].push( documents ) unless documents.empty?
           
-          #split_id = patient[ 'value' ].split('^^^')
-          #patient_id = PatientIdentifier.find( :first, 
-          #  :conditions => {  :patient_identifier => split_id.first, 
-          #                    :affinity_domain => split_id.second }
-         # )
-        
-         # p = Patient.find( patient_id.patient_id) unless patient_id.nil?
-         # @laika_patients[ patient ] = p
-          
+   
+             split_id = patient[ 'value' ].split('^^^')
+              patient_id = PatientIdentifier.find( :first, 
+                :conditions => {  :patient_identifier => split_id.first, 
+                                  :affinity_domain => split_id.second }
+              ).andand.patient_id
+
+              @laika_patients[ patient ] = Patient.find( patient_id ) unless patient_id.nil?
       end
-      
-      patient_ids = []
-      @patients.each do |patient|
-        split_id = patient[ 'value' ].split('^^^')
-        patient_id = PatientIdentifier.find( :first, 
-          :conditions => {  :patient_identifier => split_id.first, 
-                            :affinity_domain => split_id.second }
-        ).andand.patient_id
-        
-        @laika_patients[ patient ] = Patient.find( patient_id ) unless patient_id.nil?
-      end
-      
-      #@laika_patients = Patient.find( patient_ids )
+  
       
       
   end
