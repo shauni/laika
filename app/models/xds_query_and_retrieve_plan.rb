b47cb384a7b072215418ec6a4ad3939155846767
Zack Hobson <zack@opensourcery.com>
2009-08-17 09:32:22 -0700

    Test plan refactor: xds q&r flow.

--- app/models/xds_query_and_retrieve_plan.rb
+++ app/models/xds_query_and_retrieve_plan.rb
@@ -1,3 +1,6 @@
 class XdsQueryAndRetrievePlan < XdsPlan
   test_name "XDS Query & Retrieve"
+  manual_inspection
+  pending_actions 'query checklist' => :xds_checklist,
+    'document checklist' => :c32_checklist
 end
