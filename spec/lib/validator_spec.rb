86ac7654a517457c7abfaaac48c4290561e33805
Andy Gregorowicz <andy@mitre.org>
2009-08-21 14:47:30 -0400

    Added the ability to find out of a validation contains a certain kind of validation cleaned up the spec mild cleaning of the validation class

--- spec/lib/validator_spec.rb
+++ spec/lib/validator_spec.rb
@@ -1,26 +1,41 @@
 require File.dirname(__FILE__) + '/../spec_helper'
+
 require 'lib/validation.rb'
 require 'lib/validators/c32_validator.rb'
 require 'lib/validators/schema_validator.rb'
 require 'lib/validators/schematron_validator.rb'
 require 'lib/validators/umls_validator.rb'
+
 C32_SCHEMA_VALIDATOR = Validators::Schema::Validator.new("C32 Schema Validator", "resources/schemas/infrastructure/cda/C32_CDA.xsd")
 C32_SCHEMATRON_VALIDATOR = Validators::Schematron::CompiledValidator.new("C32 Schematron Validator","resources/schematron/c32_v2.1_errors.xslt")
 CCD_SCHEMATRON_VALIDATOR = Validators::Schematron::CompiledValidator.new("CCD Schematron Validator","resources/schematron/ccd_errors.xslt")
 C32_CONTENT_VALIDAOTR = Validators::C32Validation::Validator.new
 UMLS_VALIDAOTR = Validators::Umls::UmlsValidator.new("warning")
+
 describe Validation::Validator do
-  it "should be able to register  validators" do 
-    
-    #Validation.register_validator :c32, Validators::Umls::UmlsValidator.instance
+
+  before do
     Validation.register_validator :C32, C32_CONTENT_VALIDAOTR
     Validation.register_validator :C32, C32_SCHEMA_VALIDATOR
     Validation.register_validator :C32, CCD_SCHEMATRON_VALIDATOR
     Validation.register_validator :C32, C32_SCHEMATRON_VALIDATOR
+  end
 
-    Validation.get_validator(:c32).should_not be_nil
-    Validation.get_validator(:c32).validators.length.should == 4
-    
+  it "should be able to register validators" do 
+    Validation.get_validator(:C32).should_not be_nil
+    Validation.get_validator(:C32).validators.length.should == 4
+  end
+  
+  it "should be able to tell if it contains a specific validator" do
+    validator = Validation.get_validator(:C32)
+    validator.contains?(C32_SCHEMATRON_VALIDATOR).should_not be_nil
+    validator.contains?(UMLS_VALIDAOTR).should be_false
+  end
+  
+  it "should be able to tell if it contains a specific kind of validator" do
+    validator = Validation.get_validator(:C32)
+    validator.contains?(Validators::Schematron::CompiledValidator).should_not be_nil
+    validator.contains?(Validators::Umls::UmlsValidator).should be_false
   end
 
   describe "with built-in records" do
@@ -40,12 +55,7 @@ describe Validation::Validator do
   severity_terms supports telecoms users vaccines vendors zip_codes vendor_test_plans
     ]
 
-    before do
-      Validation.register_validator :C32, C32_CONTENT_VALIDAOTR
-      Validation.register_validator :C32, C32_SCHEMA_VALIDATOR
-      Validation.register_validator :C32, CCD_SCHEMATRON_VALIDATOR
-      Validation.register_validator :C32, C32_SCHEMATRON_VALIDATOR
-    end
+
 
     [ :david_carter, :emily_jones, :jennifer_thompson, :theodore_smith, :joe_smith, :will_haynes ].each do |patient|
       it "should round-trip validate #{patient} without errors or warnings" do
