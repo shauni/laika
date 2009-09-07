643dbad126cd3b4dbdef8f379c1cbb4004692ef1
Zack Hobson <zack@opensourcery.com>
2009-09-07 12:22:00 -0700

    PIX Feed: Tests for model and controller actions.
    While investigating SF #2845004 I cleaned up and wrote tests for the PIX
    Feed plan and actions.
    
     * app/models/pix_feed_plan.rb
       - add expected= accessor for assigning expected values
       - implement matches_expected? to encapsulate test logic
       - add documentation for all methods and controller actions
     * spec/controllers/test_plans_controller_spec.rb
       - add pix feed controller tests
     * spec/models/pix_feed_plan_spec.rb
       - add pix feed unit tests

--- spec/controllers/test_plans_controller_spec.rb
+++ spec/controllers/test_plans_controller_spec.rb
@@ -15,12 +15,6 @@ describe TestPlansController do
       @vendor = Vendor.factory.create(:user => @user)
       @patient = Patient.factory.create
       @controller.stub(:current_user).and_return(@user)
-      Laika::TEST_PLAN_TYPES['My Complex Test'] = MyComplexTestPlan
-      Laika::TEST_PLAN_TYPES['My Simple Test'] = MySimpleTestPlan
-    end
-    after do
-      Laika::TEST_PLAN_TYPES.delete 'My Complex Test'
-      Laika::TEST_PLAN_TYPES.delete 'My Simple Test'
     end
 
     it "should forward to the current vendor" do
@@ -29,34 +23,75 @@ describe TestPlansController do
       response.should redirect_to(vendor_test_plans_url(@vendor))
     end
 
-    it "should display tests for the specified vendor" do
-      plan = MySimpleTestPlan.create \
-        :user_id => @user.id.to_s,
-        :vendor_id => @vendor.id.to_s
-      get :index, :vendor_id => @vendor.id
-      assigns(:test_plans).to_a.should == [ plan ]
-      assigns(:vendor).should == @vendor
-      assigns(:other_vendors).should == []
+    describe "with a PIX Feed plan" do
+      before do
+        @plan = PixFeedPlan.factory.create
+      end
+
+      it "should request additional data" do
+        get :pix_feed_setup, :id => @plan.id
+        response.should be_success
+        response.should render_template('test_plans/pix_feed_setup.html.erb')
+      end
+
+      it "should compare results and fail" do
+        post :pix_feed_compare, :id => @plan.id,
+          :patient_identifier => {
+            :patient_identifier => 'foo',
+            :affinity_domain => 'bar'
+          }
+        @plan.reload
+        @plan.should be_failed
+      end
+
+      it "should compare results and pass" do
+        pi = { :patient_identifier => 'foo', :affinity_domain => 'bar' }
+        @plan.patient.patient_identifiers << PatientIdentifier.new(pi)
+        post :pix_feed_compare, :id => @plan.id, :patient_identifier => pi
+        @plan.reload
+        @plan.should be_passed
+      end
     end
 
-    it "should assign a simple test" do
-      old_count = TestPlan.count
-      post :create, :test_plan => {
-          :type => 'MySimpleTestPlan',
-          :user_id => @user.id.to_s,
-          :vendor_id => @vendor.id.to_s
-        }, :patient_id => @patient.id.to_s
-      TestPlan.count.should == old_count + 1
-    end
+    describe "with test-specific plans" do
+      before(:all) do
+        Laika::TEST_PLAN_TYPES['My Complex Test'] = MyComplexTestPlan
+        Laika::TEST_PLAN_TYPES['My Simple Test'] = MySimpleTestPlan
+      end
+      after(:all) do
+        Laika::TEST_PLAN_TYPES.delete 'My Complex Test'
+        Laika::TEST_PLAN_TYPES.delete 'My Simple Test'
+      end
 
-    it "should request more info for a complex test" do
-      old_count = TestPlan.count
-      post :create, :test_plan => {
-          :type => 'MyComplexTestPlan',
+      it "should display tests for the specified vendor" do
+        plan = MySimpleTestPlan.create \
           :user_id => @user.id.to_s,
           :vendor_id => @vendor.id.to_s
-        }, :patient_id => @patient.id.to_s
-      TestPlan.count.should == old_count
+        get :index, :vendor_id => @vendor.id
+        assigns(:test_plans).to_a.should == [ plan ]
+        assigns(:vendor).should == @vendor
+        assigns(:other_vendors).should == []
+      end
+
+      it "should assign a simple test" do
+        old_count = TestPlan.count
+        post :create, :test_plan => {
+            :type => 'MySimpleTestPlan',
+            :user_id => @user.id.to_s,
+            :vendor_id => @vendor.id.to_s
+          }, :patient_id => @patient.id.to_s
+        TestPlan.count.should == old_count + 1
+      end
+
+      it "should request more info for a complex test" do
+        old_count = TestPlan.count
+        post :create, :test_plan => {
+            :type => 'MyComplexTestPlan',
+            :user_id => @user.id.to_s,
+            :vendor_id => @vendor.id.to_s
+          }, :patient_id => @patient.id.to_s
+        TestPlan.count.should == old_count
+      end
     end
   end
 end
