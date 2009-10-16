58d1101e2122b1144a762b249610bd340987c7a6
Indranrita Deshmukh <ideshmukh@ideshmukh-macbookpro.mitre.org>
2009-10-16 11:18:11 -0400

    more import code

--- config/initializers/laika_validation.rb
+++ config/initializers/laika_validation.rb
@@ -13,5 +13,8 @@ Validation.register_validator :C32, Validators::Umls::UmlsValidator.new("warning
 
 require 'import_helper'
 require 'importers/c32/allergy_c32_importer'
+require 'importers/c32/registration_information_c32_importer'
+require 'importers/c32/condition_c32_importer'
+require 'importers/c32/patient_c32_importer'
 
 require 'active_record_comparator'
