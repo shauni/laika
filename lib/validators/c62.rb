3c0f6c385535082675b41b6c63bf6c3284f9283b
Zack Hobson <zack@opensourcery.com>
2009-10-30 13:20:36 -0700

    Started C62 content inspection, not yet complete.

--- lib/validators/c62.rb
+++ lib/validators/c62.rb
@@ -1,15 +1,17 @@
 module Validators
   module C62
     class Reader
-      # @param xml_data the raw payload contents
+      # @param [String] xml_data C62 XML
       def initialize xml_data
         @elements = REXML::Document.new(xml_data).elements
       end
 
       private
 
+      attr_reader :elements
+
       def payload_element
-        @elements['ClinicalDocument/component/nonXMLBody/text']
+        elements['ClinicalDocument/component/nonXMLBody/text']
       end
 
       public
@@ -26,13 +28,105 @@ module Validators
     end
 
     class Validator < Validation::BaseValidator
+      TEMPLATE_ID_ROOT = '1.3.6.1.4.1.19376.1.2.20'
+
       def validate patient, document
-         [ContentError.new(:section         => 'C62',
-                           :error_message   => 'Not yet implemented.',
-                           :type            =>'error',
-                           :location        => nil,
-                           :validator       => 'C62 Validator',
-                           :inspection_type => ::CONTENT_INSPECTION)]
+        elements = document.elements
+        [].tap do |errors|
+          # The ClinicalDocument/templateId element shall be present. The root
+          # attribute shall contain the oid, ‘1.3.6.1.4.1.19376.1.2.20’, to
+          # indicate this document is an XDS-SD document.
+          errors.concat(check_required(elements, 'ClinicalDocument/templateId',
+                                       %w[ root ]) do |templateId|
+            if templateId.attributes['root'] != TEMPLATE_ID_ROOT
+              errors << inspection_error(templateId.xpath,
+                'ClinicalDocument/templateId root attribute does not match expected.')
+            end
+          end)
+
+          # The ClinicalDocument/id element shall be present. The root
+          # attribute shall contain the oid for the document, in which case
+          # the extension attribute shall be empty, or an oid that scopes the
+          # set of possible unique values for the extension attribute, in which
+          # case the extension shall be populated with a globally unique
+          # identifier within the scope of the root oid.
+          errors.concat check_required(elements, 'ClinicalDocument/id', %w[ root ])
+
+          # The ClinicalDocument/code will in most cases be provided by the
+          # operator. Values for this code are dictated by the CDA R2
+          # documentation, but are permissible to extend to fit the particular
+          # use case. Attributes code@code and code@codeSystem shall be present.
+          errors.concat check_required(elements, 'ClinicalDocument/code',
+                                        %w[code codeSystem])
+
+          # The ClinicalDocument/effectiveTime shall denote the time at which
+          # the original content was scanned. At a minimum, the time shall be
+          # precise to the day and shall include the time zone offset from GMT.
+          errors.concat check_required(elements, 'ClinicalDocument/effectiveTime')
+
+          # The ClinicalDocument/confidentialityCode shall be assigned by the
+          # operator in accordance with the scanning facility policy. ...
+          # Attributes confidentialityCode@code and confidentialityCode@codeSystem
+          # shall be present.
+          errors.concat check_required(elements,
+                                        'ClinicalDocument/confidentialityCode',
+                                        %w[ code codeSystem ])
+
+          # The ClinicalDocument/component/nonXMLBody/text element shall be
+          # present and encoded using xs:base64Binary encoding. Its #CDATA
+          # will contain the scanned content.
+          # ClinicalDocument/component/nonXMLBody/text@mediaType shall be
+          # “application/pdf” for PDF, or “text/plain” for plaintext.
+          # ClinicalDocument/component/nonXMLBody/text@representation shall
+          # be present. The @representation for both PDF and plaintext scanned
+          # content will be “B64”, because this profile requires the base-64
+          # encoding of both formats.
+          errors.concat(check_required(elements,
+                                        'ClinicalDocument/component/nonXMLBody/text',
+                                        %w[ mediaType representation ]) do |text|
+            if not %w[text/plain application/pdf].include? text.attributes['mediaType']
+              errors << inspection_error(text.xpath,
+                "#{text.xpath}@mediaType attribute does not match expected.")
+            end
+            if text.attributes['representation'] != 'B64'
+              errors << inspection_error(text.xpath,
+                "#{text.xpath}@representation attribute does not match expected.")
+            end
+          end)
+
+          errors.compact! # remove nils
+        end
+      end
+
+      private
+
+      def inspection_error xpath, message
+        ContentError.new(
+          :error_message   => message,
+          :type            => 'error',
+          :location        => xpath,
+          :validator       => 'C62 Validator',
+          :inspection_type => ::CONTENT_INSPECTION)
+      end
+
+      def check_required elements, xpath, attr_names = []
+        [].tap do |errors|
+          element = elements[xpath]
+          if element.nil?
+            errors << inspection_error('ClinicalDocument',
+              "Missing required element #{xpath}.")
+          else
+            attr_names.each do |attr_name|
+              if element.attributes[attr_name].nil?
+                errors << inspection_error(element.xpath,
+                  "#{element.xpath}@#{attr_name} attribute is not present.")
+              end
+            end
+            if block_given?
+              yield element
+            end
+          end
+        end
       end
     end
   end
---
+++ spec/lib/c62_validator_spec.rb
@@ -0,0 +1,74 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe Validators::C62::Validator do
+  before do
+    xml = <<-END_C62
+<ClinicalDocument>
+<templateId root="1.3.6.1.4.1.19376.1.2.20"/>
+<id root="1.3.4.2.5.2.3.4.2"/>
+<title>My Clinical Document</title>
+<code code="foo" codeSystem="bar"/>
+<confidentialityCode code="foo" codeSystem="bar"/>
+<effectiveTime>2009-10-28 00:00:00 -0800</effectiveTime>
+<component><nonXMLBody>
+<text mediaType="text/plain" representation="B64">
+SGkgd29ybGQhCg==
+</text></nonXMLBody></component>
+</ClinicalDocument>
+    END_C62
+    @doc = REXML::Document.new xml
+    @patient = Patient.factory.create
+    @validator = Validators::C62::Validator.new
+  end
+
+  it "should pass a 'quote' valid 'unquote' C62" do
+    @validator.validate(@patient, @doc).should be_empty
+  end
+
+  it "should require the correct template ID" do
+    @doc.elements['ClinicalDocument/templateId'].attributes['root'] = 'foobar'
+    @validator.validate(@patient, @doc).should_not be_empty
+  end
+
+  it "should require the correct media type" do
+    @doc.elements['ClinicalDocument/component/nonXMLBody/text'].
+      attributes['mediaType'] = 'foobar'
+    @validator.validate(@patient, @doc).should_not be_empty
+  end
+
+  it "should require the correct representation" do
+    @doc.elements['ClinicalDocument/component/nonXMLBody/text'].
+      attributes['representation'] = 'foobar'
+    @validator.validate(@patient, @doc).should_not be_empty
+  end
+
+  %w[
+    ClinicalDocument/id
+    ClinicalDocument/templateId
+    ClinicalDocument/effectiveTime
+    ClinicalDocument/code
+    ClinicalDocument/confidentialityCode
+    ClinicalDocument/component/nonXMLBody/text
+  ].each do |xpath|
+    it "should require element #{xpath}" do
+      @doc.elements.delete xpath
+      @validator.validate(@patient, @doc).should_not be_empty
+    end
+  end
+
+  %w[
+    ClinicalDocument/id@root
+    ClinicalDocument/templateId@root
+    ClinicalDocument/code@code
+    ClinicalDocument/code@codeSystem
+    ClinicalDocument/confidentialityCode@code
+    ClinicalDocument/confidentialityCode@codeSystem
+    ClinicalDocument/component/nonXMLBody/text@mediaType
+    ClinicalDocument/component/nonXMLBody/text@representation
+  ].map {|a| a.split(/@/, 2) }.each do |element, attribute|
+    it "should require attribute #{element}@#{attribute}" do
+      @doc.elements[element].attributes.delete(attribute)
+      @validator.validate(@patient, @doc).should_not be_empty
+    end
+  end
+end
