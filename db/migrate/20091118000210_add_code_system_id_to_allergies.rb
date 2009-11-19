1ff9186b30ad4f0806a2577d45ff828b785a0b95
Josh Partlow <jpartlow@opensourcery.com>
2009-11-18 16:51:33 -0800

    * allergies/edit minor text change to coding instruction  * db/migrate/20091118000210_add_code_system_id_to_alelrgies.rb - existing allergy rows need to have a code system set (NOTE this does change an existing migration from a few commits back...)

--- db/migrate/20091118000210_add_code_system_id_to_allergies.rb
+++ db/migrate/20091118000210_add_code_system_id_to_allergies.rb
@@ -1,6 +1,19 @@
 class AddCodeSystemIdToAllergies < ActiveRecord::Migration
+ 
+  class Allergy < ActiveRecord::Base
+    belongs_to :code_system
+  end
+
+  class CodeSystem < ActiveRecord::Base; end
+
   def self.up
     add_column :allergies, :code_system_id, :integer
+    # Set all allergies without a code system to RxNorm
+    code_system = CodeSystem.find_by_code('2.16.840.1.113883.6.88')
+    Allergy.find_all_by_code_system_id(nil).each do |a|
+      a.code_system = code_system
+      a.save!
+    end
   end
 
   def self.down
