4ae97867db7670fe0df3ab7312cf773934417618
Rob Dingwell <rob@robdingwell.org>
2009-12-11 09:18:42 -0500

    Fixing additonal merge issue not seen on the first go around

--- lib/validation.rb
+++ lib/validation.rb
@@ -150,47 +150,48 @@ require_dependency 'validators/schematron_validator'
 require_dependency 'validators/umls_validator'
 require_dependency 'validators/xds_metadata_validator'
 
+
 {
   'C32 v2.1/v2.3' => [
     Validators::C32Validation::Validator.new,
     Validators::Schema::Validator.new("C32 Schema Validator",
-      "resources/schemas/infrastructure/cda/C32_CDA.xsd"),
+      "#{RAILS_ROOT}/resources/schemas/infrastructure/cda/C32_CDA.xsd"),
     Validators::Schematron::CompiledValidator.new("CCD Schematron Validator",
-      "resources/schematron/ccd_errors.xslt"),
+      "#{RAILS_ROOT}/resources/schematron/ccd_errors.xslt"),
     Validators::Schematron::CompiledValidator.new("C32 Schematron Validator",
-      "resources/schematron/c32_v2.1/c32_v2.1_errors.xslt"),
+      "#{RAILS_ROOT}/resources/schematron/c32_v2.1/c32_v2.1_errors.xslt"),
     Validators::Umls::UmlsValidator.new("warning")
   ],
   'C32 v2.4' => [
     Validators::C32Validation::Validator.new,
     Validators::Schema::Validator.new("C32 Schema Validator",
-      "resources/schemas/infrastructure/cda/C32_CDA.xsd"),
+      "#{RAILS_ROOT}/resources/schemas/infrastructure/cda/C32_CDA.xsd"),
     Validators::Schematron::CompiledValidator.new("CCD Schematron Validator",
-      "resources/schematron/ccd_errors.xslt"),
+      "#{RAILS_ROOT}/resources/schematron/ccd_errors.xslt"),
     Validators::Schematron::CompiledValidator.new("C32 Schematron Validator",
-      "resources/schematron/c32_v2.4/c32_v2.4_errors.xslt"),
+      "#{RAILS_ROOT}/resources/schematron/c32_v2.4/c32_v2.4_errors.xslt"),
     Validators::Umls::UmlsValidator.new("warning")
   ],
   'C32 v2.5' => [
     Validators::C32Validation::Validator.new,
     Validators::Schema::Validator.new("C32 Schema Validator",
-      "resources/schemas/infrastructure/cda/C32_CDA.xsd"),
+      "#{RAILS_ROOT}/resources/schemas/infrastructure/cda/C32_CDA.xsd"),
     Validators::Schematron::CompiledValidator.new("CCD Schematron Validator",
-      "resources/schematron/ccd_errors.xslt"),
+      "#{RAILS_ROOT}/resources/schematron/ccd_errors.xslt"),
     Validators::Schematron::CompiledValidator.new("C32 Schematron Validator",
-      "resources/schematron/c32_v2.5/c32_v2.5_errors.xslt"),
+      "#{RAILS_ROOT}/resources/schematron/c32_v2.5/c32_v2.5_errors.xslt"),
     Validators::Umls::UmlsValidator.new("warning")
   ],
   'NHIN C32' => [
     Validators::C32Validation::Validator.new,
     Validators::Schema::Validator.new("C32 Schema Validator",
-      "resources/schemas/infrastructure/cda/C32_CDA.xsd"),
+      "#{RAILS_ROOT}/resources/schemas/infrastructure/cda/C32_CDA.xsd"),
     Validators::Schematron::CompiledValidator.new("CCD Schematron Validator",
-      "resources/schematron/ccd_errors.xslt"),
+      "#{RAILS_ROOT}/resources/schematron/ccd_errors.xslt"),
     Validators::Schematron::CompiledValidator.new("C32 Schematron Validator",
-      "resources/schematron/c32_v2.1_errors.xslt"),
+      "#{RAILS_ROOT}/resources/schematron/c32_v2.1_errors.xslt"),
     Validators::Schematron::CompiledValidator.new("NHIN Schematron Validator",
-      "resources/nhin_schematron/nhin_errors.xsl"),
+      "#{RAILS_ROOT}/resources/nhin_schematron/nhin_errors.xsl"),
     Validators::Umls::UmlsValidator.new("warning")
   ],
   'C62' => [ Validators::C62::Validator.new ]
@@ -198,5 +199,6 @@ require_dependency 'validators/xds_metadata_validator'
   validators.each do |validator|
     Validation.register_validator type.to_sym, validator
   end
+
 end
 
