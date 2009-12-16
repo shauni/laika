cb061068682541dc6b80d1f5d93b107da72b0886
Josh Partlow <jpartlow@opensourcery.com>
2009-12-16 15:23:31 -0800

    removeing duplicate xds provide_and_register actions from routes

--- config/routes.rb
+++ config/routes.rb
@@ -32,13 +32,6 @@ ActionController::Routing::Routes.draw do |map|
     xds_patients.do_provide_and_register_xds_patient '/xds_patients/do_provide_and_register', :action => 'do_provide_and_register'
   end
   
-
-
-  map.with_options :controller => 'xds_patients' do |xds_patients|
-    xds_patients.provide_and_register_xds_patient '/xds_patients/provide_and_register/:id', :action => 'provide_and_register'
-    xds_patients.do_provide_and_register_xds_patient '/xds_patients/do_provide_and_register', :action => 'do_provide_and_register'
-  end
-
   map.with_options :controller => 'account' do |account|
     %w[ signup login logout forgot_password reset_password ].each do |action|
       account.send(action, "/account/#{action}", :action => action)
