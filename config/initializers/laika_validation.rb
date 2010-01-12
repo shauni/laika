edf9afbfa3b64c36aa07bbfdb20772f7f99ca273
Josh Partlow <jpartlow@opensourcery.com>
2010-01-12 12:23:11 -0800

    Removed validator initialization for C32 v2.4 and C62. closes #123

--- config/initializers/laika_validation.rb
+++ config/initializers/laika_validation.rb
@@ -107,16 +107,6 @@ require 'validators/xds_metadata_validator'
       "#{RAILS_ROOT}/resources/schematron/c32_v2.1/c32_v2.1_errors.xslt"),
     Validators::Umls::UmlsValidator.new("warning")
   ],
-  'C32 v2.4' => [
-    Validators::C32Validation::Validator.new,
-    Validators::Schema::Validator.new("C32 Schema Validator",
-      "#{RAILS_ROOT}/resources/schemas/infrastructure/cda/C32_CDA.xsd"),
-    Validators::Schematron::CompiledValidator.new("CCD Schematron Validator",
-      "#{RAILS_ROOT}/resources/schematron/ccd_errors.xslt"),
-    Validators::Schematron::CompiledValidator.new("C32 Schematron Validator",
-      "#{RAILS_ROOT}/resources/schematron/c32_v2.4/c32_v2.4_errors.xslt"),
-    Validators::Umls::UmlsValidator.new("warning")
-  ],
   'C32 v2.5' => [
     Validators::C32Validation::Validator.new,
     Validators::Schema::Validator.new("C32 Schema Validator",
@@ -139,7 +129,6 @@ require 'validators/xds_metadata_validator'
       "#{RAILS_ROOT}/resources/nhin_schematron/nhin_errors.xsl"),
     Validators::Umls::UmlsValidator.new("warning")
   ],
-  'C62' => [ Validators::C62::Validator.new ]
 }.each do |type, validators|
   validators.each do |validator|
     Validation.register_validator type.to_sym, validator
